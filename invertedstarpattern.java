63ba8a0012213e04d727aea9f69739b31e3bbc6e
Deepak26-tech <deepakgupta26062005@gmail.com>
2026-06-20 23:35:37 +0530

    add files

---
+++ invertedstarpattern.java
@@ -0,0 +1,29 @@
+
+
+import java.util.Scanner;
+
+//****
+//***
+//**
+//* 
+public class invertedstarpattern  {
+
+    
+}
+
+public class invertedstarpattern {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the number n :");
+        int n = sc.nextInt();
+
+
+        for(int i=1; i<=n; i++){
+            for(int j=1;j<=n-i+1;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+              
+        }
+    }
+}
